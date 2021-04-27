bbca0659ef69ace37ed977d479b53aca23ad0289
SFrueh <shawn.frueh@nike.com>
2021-04-26 22:19:16 -0700

    Initial Tests

---
+++ test_visualize.py
@@ -0,0 +1,31 @@
+from os.path import abspath
+from PIL import Image
+import random
+
+image_name = abspath("grid.png")
+img = Image.new("RGB", size=(400, 600), color=(0, 200, 0))
+
+
+def print_grid(bounds, width=40, height=60):
+    rand_color = tuple(random.randint(0, 255) for i in range(3))
+    print(rand_color)
+    for x in range(height):
+        for y in range(width):
+            if bounds[0] <= x <= bounds[1]:
+                color = rand_color
+                img.putpixel(xy=(y, x), value=color)
+            elif bounds[2] <= y <= bounds[3]:
+                color = rand_color
+                img.putpixel(xy=(y, x), value=color)
+            else:
+                color = (209, 123, 193)
+
+
+data = [[int(j) for j in i.split(" ")] for i in ("0 292 399 307",)]
+data = [[int(j) for j in i.split(" ")] for i in ("48 192 351 207", "48 392 351 407", "120 52 135 547", "260 52 275 547")]
+print(data)
+
+for d in data:
+    print_grid(d, width=400, height=600)
+
+img.show()
